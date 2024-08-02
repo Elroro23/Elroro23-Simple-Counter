40fdb667a63526d8d08157bf8511317ca37619c4
Elroro23 <rodolfodalessandro23@gmail.com>
2024-08-02 14:10:32 +0000

    Proyecto realizado con sus respectivos pasos.

--- src/js/component/Count.js
+++ src/js/component/Count.js
@@ -1,7 +1,7 @@
 
 import React from "react"
-const Count = ({ seconds }) => {
-    //logica js.
+const Count = ({ seconds }) => { //Definimos un componente con "seconds" como prop.
+//Dividimos seconds por 1000, tomamos la parte entera con Math.floor, y luego obtenemos el último dígito con % 10. Así extraemos el cuarto dígito desde la derecha.
     const four = Math.floor(seconds / 1000) % 10;
     const three = Math.floor(seconds / 100) % 10;
     const two = Math.floor(seconds / 10) % 10;
@@ -12,7 +12,7 @@ const Count = ({ seconds }) => {
                 <div className="col icon">
                     <i className="fa-regular fa-clock"></i>
                 </div>
-                <div className="col digits">{four}
+                <div className="col digits">{four} 
                 </div>
                 <div className="col digits">{three}
                 </div>
--- src/js/index.js
+++ src/js/index.js
@@ -8,9 +8,10 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/Home.js";
 
-let seconds = 0;
-setInterval(function () {
-    seconds++;
-    ReactDOM.createRoot(document.getElementById('app')).render(<Home seconds={seconds} />);
-}, 1000);
+let seconds = 0; //Inicializamos "seconds" en 0;
+//"setInterval" se utiliza para ejecutar una función de manera repetitiva en intervalos de tiempo específicos. En este caso, la función se ejecuta cada 1000 milisegundos (1 segundo).
+setInterval(function () { //"function" es una función anónima dentro de "setInterval" para definir dos cosas:
+    seconds++;//Aumentar el valor de seconds en 1 cada vez que la función se ejecuta.
+    ReactDOM.createRoot(document.getElementById('app')).render(<Home seconds={seconds} />); //Y renderizando "home" con "seconds como prop".
+}, 1000); //1 segundo.
 
